e579149cd881400291765760d6e6a8ffc6f813de
Ryan Murphy <rdmurphy@users.noreply.github.com>
2019-03-20 23:16:39 -0500

    Only create the auth in test once

--- tests/test.js
+++ tests/test.js
@@ -8,15 +8,16 @@ const expectedExtensionsOutput = require('./expected/extensions');
 const basicDocumentId = '1coln1etP5rT1MqmNtRT7lToGtCi1EAsDVzC5aq0LsIc';
 const extensionsDocumentId = '1_v0gAswpNnGnDqAx7cU_1bFEK8J7fi8EBvfKvgGZubc';
 
-const getAuth = async () =>
-  await google.auth.getClient({
-    scopes: ['https://www.googleapis.com/auth/documents.readonly'],
+describe('@newswire/doc-to-archieml', () => {
+  let auth;
+
+  before(async () => {
+    auth = await google.auth.getClient({
+      scopes: ['https://www.googleapis.com/auth/documents.readonly'],
+    });
   });
 
-describe('@newswire/doc-to-archieml', () => {
   it('should return a valid JavaScript object of basic ArchieML data', async () => {
-    const auth = await getAuth();
-
     const actual = await docToArchieML({
       documentId: basicDocumentId,
       auth,
@@ -26,8 +27,6 @@ describe('@newswire/doc-to-archieml', () => {
   });
 
   it('should return a valid JavaScript object of extended ArchieML data', async () => {
-    const auth = await getAuth();
-
     const actual = await docToArchieML({
       documentId: extensionsDocumentId,
       auth,
