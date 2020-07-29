e46f3dbcadaa454a207a87ed024ea43ec91fb9a8
Ryan Murphy <rdmurphy@users.noreply.github.com>
2020-07-29 02:40:11 -0700

    Move to uvu, add in coverage

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,12 @@
-const { strict: assert } = require('assert');
+// packages
 const { GoogleAuth } = require('google-auth-library');
+const { test: it } = require('uvu');
+const assert = require('uvu/assert');
+
+// library
 const { docToArchieML } = require('../');
 
+// fixtures
 const expectedBasicOutput = require('./expected/basic');
 const expectedExtensionsOutput = require('./expected/extensions');
 
@@ -12,40 +17,40 @@ const extensionsDocumentId =
   process.env.EXTENSIONS_DOCUMENT_ID ||
   '1_v0gAswpNnGnDqAx7cU_1bFEK8J7fi8EBvfKvgGZubc';
 
-describe('@newswire/doc-to-archieml', () => {
-  let client;
-
-  before(async () => {
-    const auth = new GoogleAuth({
-      scopes: ['https://www.googleapis.com/auth/documents.readonly'],
-    });
-
-    client = await auth.getClient();
-  });
-
-  it('should return a valid JavaScript object of basic ArchieML data', async () => {
-    const actual = await docToArchieML({
-      documentId: basicDocumentId,
-      client,
-    });
-
-    assert.deepStrictEqual(actual, expectedBasicOutput);
-  });
-
-  it('should return a valid JavaScript object of extended ArchieML data', async () => {
-    const actual = await docToArchieML({
-      documentId: extensionsDocumentId,
-      client,
-    });
-
-    assert.deepStrictEqual(actual, expectedExtensionsOutput);
-  });
-
-  it('should be able to set up a minimal client if one is not passed', async () => {
-    const actual = await docToArchieML({
-      documentId: basicDocumentId,
-    });
-
-    assert.deepStrictEqual(actual, expectedBasicOutput);
-  });
+const auth = new GoogleAuth({
+  scopes: ['https://www.googleapis.com/auth/documents.readonly'],
 });
+
+let client;
+
+it.before(async () => {
+  client = await auth.getClient();
+});
+
+it('should return a valid JavaScript object of basic ArchieML data', async () => {
+  const actual = await docToArchieML({
+    documentId: basicDocumentId,
+    client,
+  });
+
+  assert.equal(actual, expectedBasicOutput);
+});
+
+it('should return a valid JavaScript object of extended ArchieML data', async () => {
+  const actual = await docToArchieML({
+    documentId: extensionsDocumentId,
+    client,
+  });
+
+  assert.equal(actual, expectedExtensionsOutput);
+});
+
+it('should be able to set up a minimal client if one is not passed', async () => {
+  const actual = await docToArchieML({
+    documentId: basicDocumentId,
+  });
+
+  assert.equal(actual, expectedBasicOutput);
+});
+
+it.run();
